8348588665081a7eedb8e1bd8afb4b6092c3d045
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2016-03-09 11:37:34 -0800

    code

---
+++ main.js
@@ -0,0 +1,26 @@
+
+// $('.nav-tabs.juttle-service a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// });
+
+$('.nav-tabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+$("#log-example-adapter").typed({
+  strings: ["elastic", "file", "splunk"],
+  loop: true,
+  showCursor: false,
+  typeSpeed: 0,
+  backDelay: 1000
+});
+
+$("#moving-average-example-adapter").typed({
+  strings: ["influx", "graphite", "opentsdb"],
+  loop: true,
+  showCursor: false,
+  typeSpeed: 0,
+  backDelay: 1000
+});
