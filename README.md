34be7fff3a6bc76f2a4b239d35544ec2326db382
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2016-03-09 11:37:34 -0800

    code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # juttle.github.io
+
+## To view
+```bash
+$ open index.html
+```
+
+## To develop
+Set up sass to watch and recompile the sass file
+```
+$ gem install sass
+$ sass --watch .:dist
+```
---
+++ main.js
@@ -0,0 +1,16 @@
+
+$("#log-example-adapter").typed({
+  strings: ["elastic", "file", "splunk"],
+  loop: true,
+  showCursor: false,
+  typeSpeed: 0,
+  backDelay: 1000
+});
+
+$("#moving-average-example-adapter").typed({
+  strings: ["influx", "graphite", "opentsdb"],
+  loop: true,
+  showCursor: false,
+  typeSpeed: 0,
+  backDelay: 1000
+});
